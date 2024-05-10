586f72e30158663adfce7c56bd2a4fb3bb83ccb3
Serhii Kobeza <kobeza.sv@gmail.com>
2024-05-10 11:37:11 +0100

    Initial Commit

---
+++ EmojiSelector/EmojiSelectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiSelectorApp.swift
+//  EmojiSelector
+//
+//  Created by Serhii Kobeza on 10.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct EmojiSelectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
